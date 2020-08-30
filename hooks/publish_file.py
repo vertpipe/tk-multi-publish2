cd7f31023127bb57c5e803634c0f712802c47ede
Bo Kamphues <b.kamphues@outlook.com>
2020-08-30 10:31:50 +0200

    Added extra logging

--- hooks/publish_file.py
+++ hooks/publish_file.py
@@ -568,6 +568,8 @@ class BasicFilePublishPlugin(HookBaseClass):
         if work_template and publish_template:
             if work_template.validate(path):
                 work_fields = work_template.get_fields(path)
+            else:
+                self.logger.warning("Could not validate work_template property, check if it is configured correctly.")
 
             missing_keys = publish_template.missing_keys(work_fields)
 
